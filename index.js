3f7f6e41c5c1a393312af060d4418ab7ddff0868
sushilvrize <SushilT@vrize.com>
2026-03-01 20:14:03 +0530

    frogress on fcm

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import { setBackgroundMessageHandler } from './src/services/NotificationService';
+import { setBackgroundMessageHandler, getFCMToken } from './src/services/NotificationService';
 
 // Must be called outside of React component tree
 setBackgroundMessageHandler();
+
+if (Platform.OS === 'android') {
+    getFCMToken().then(token => console.log('===> FCM TOKEN:', token)).catch(console.error);
+}
 
 AppRegistry.registerComponent(appName, () => App);
